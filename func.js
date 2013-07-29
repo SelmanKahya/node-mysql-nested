8e9ce9dd0913f002d10e1d17ed1cb3d1cd9710d2
Selman <selmanhalid@hotmail.com>
2013-07-29 03:04:36 -0700

    modified comments in func.js

--- func.js
+++ func.js
@@ -4,8 +4,14 @@
  * https://github.com/Selmanh
  */
 
-// Takes nested mysql result array as a parameter,
-// converts it to a nested object
+/**
+ * takes nested mysql result array as a parameter,
+ * converts it to a nested object
+ *
+ * @param {Array} rows
+ * @param {Array} nestingOptions
+ * @return {Object}
+ */
 exports.convertToNested = function (rows, nestingOptions) {
 
     if (rows == null || nestingOptions == null)
@@ -13,7 +19,7 @@ exports.convertToNested = function (rows, nestingOptions) {
 
     var levels = nestingOptions;
 
-    // Put similar objects in the same bucket (by table name)
+    // put similar objects in the same bucket (by table name)
     var buckets = new Array();
 
     for (var i = 0; i < levels.length; i++) {
@@ -32,17 +38,15 @@ exports.convertToNested = function (rows, nestingOptions) {
                 continue;
             }
 
-            // iterate result array check if object exist
-            // if object is already in result array then do nothing
-            // otherwise add it to result array
+            // if object isn't in result array, then push it
             if(!isExist(result, key, object[key]))
                 result.push(object);
         }
         buckets.push(result);
     }
 
-    // We have similar objects in the same bucket
-    // Now, move lower level objects into related upper level objects
+    // we have similar objects in the same bucket
+    // now, move lower level objects into related upper level objects
     for (var i = buckets.length-1; i >= 1; i--) {
         var relationKey = levels[i-1].key;
         var relationTable = levels[i].tableName;
@@ -53,13 +57,13 @@ exports.convertToNested = function (rows, nestingOptions) {
             var value, indexes = null;
             var object = objects[j];
 
-            // IF lower level table doesn't have upper level table's primary column as foreign key
-            // THEN lower level table has key to upper table
             // if couldn't find foreign key value in upper level, look to upper levels
             var targetBucket, a = 1;
             while(indexes == null && (i-a) >= 0){
                 targetBucket = buckets[i-a];
 
+                // if lower level table doesn't have upper level table's primary column as foreign key
+                // then upper table has a foreign key to lower table
                 if(levels[i].hasOwnProperty('hasForeignKeyToUpperTable') && !levels[i].hasForeignKeyToUpperTable)
                     relationKey = levels[i].key;
                 else
@@ -70,7 +74,7 @@ exports.convertToNested = function (rows, nestingOptions) {
                 a++;
             }
 
-            // relation to upper object not found - ignore this object
+            // relation to upper object(s) not found - ignore this object
             if(indexes == null || indexes.length == 0)
                 continue;
 
@@ -87,7 +91,16 @@ exports.convertToNested = function (rows, nestingOptions) {
     return buckets[0];
 };
 
-// Returns indexes of an object that has given key and value in objects array
+
+/**
+ * returns indexes of the object that has given key property,
+ * and the value of that property equals to given value
+ *
+ * @param {Array} objects
+ * @param {String} key
+ * @param {String} value
+ * @return {Array}
+ */
 var getIndexes = function(objects, key, value){
 
     if (objects == null || key == null || value == null)
@@ -104,7 +117,16 @@ var getIndexes = function(objects, key, value){
     return result;
 }
 
-// Checks if key and value exists in one of the objects in given array
+/**
+ * checks if one of the objects in an array has the given property,
+ * and the value of that property equals to given value
+ *
+ * @param {Array} array
+ * @param {String} key
+ * @param {String} value
+ * @return {Array}
+ */
+//
 var isExist = function(array, key, value){
 
     for (var i = 0; i < array.length; i++) {
