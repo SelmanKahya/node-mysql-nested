ba979fd500b950c36cf31aa3e44f05f197549592
Selman <selmanhalid@hotmail.com>
2013-07-28 19:03:06 -0700

    renamed createNestedJSON to convertToNested added if: checks if the object has 'key' property before pushing into array

--- func.js
+++ func.js
@@ -6,33 +6,15 @@
 
 // Takes nested mysql result array as a parameter,
 // converts it to a nested object
-exports.createNestedJSON = function (rows, nestingOptions) {
+exports.convertToNested = function (rows, nestingOptions) {
 
-    // Returns indexes of an object that has given key and value in object array
-    var getIndex = function(objects, key, value){
-        var result = new Array();
-        if (objects == null || key == null || value == null)    return null;
-        for (var i = 0; i < objects.length; i++) {
-            if(objects[i][key] == value){
-                result.push(i);
-            }
-        }
-        return result;
-    }
-
-    // Checks if key and value exists in one of the objects in given array
-    var isExist = function(array, key, value){
-        for (var i = 0; i < array.length; i++) {
-            if(array[i][key] == value)
-                return true;
-        }
-        return false;
-    }
+    if (rows == null || nestingOptions == null)
+        return rows;
 
     var levels = nestingOptions;
 
     // Put similar objects in the same bucket (by table name)
-    var separatedObjects = new Array();
+    var buckets = new Array();
 
     for (var i = 0; i < levels.length; i++) {
         var result = new Array();
@@ -44,19 +26,23 @@ exports.createNestedJSON = function (rows, nestingOptions) {
         for (var j = 0; j < rows.length; j++) {
             var object = rows[j][tableName];
 
+            // check if object has key property
+            if (object == null){
+                console.log("Error: couldn't find " + tableName + " property in mysql result set")
+                continue;
+            }
+
             // iterate result array check if object exist
             // if object is already in result array then do nothing
             // otherwise add it to result array
             if(!isExist(result, key, object[key]))
                 result.push(object);
         }
-        separatedObjects.push(result);
+        buckets.push(result);
     }
 
-    var buckets = separatedObjects;
-
     // We have similar objects in the same bucket
-    // Now concatenate lower level objects in upper level related parent objects
+    // Now, move lower level objects into related upper level objects
     for (var i = buckets.length-1; i >= 1; i--) {
         var relationKey = levels[i-1].key;
         var relationTable = levels[i].tableName;
@@ -65,10 +51,11 @@ exports.createNestedJSON = function (rows, nestingOptions) {
         for (var j = 0; j < objects.length; j++) {
 
             var value, indexes = null;
+            var object = objects[j];
 
             // IF lower level table doesn't have upper level table's primary column as foreign key
             // THEN lower level table has key to upper table
-            // if couldnt find foreign key value in upper level, look other upper levels
+            // if couldn't find foreign key value in upper level, look to upper levels
             var targetBucket, a = 1;
             while(indexes == null && (i-a) >= 0){
                 targetBucket = buckets[i-a];
@@ -78,12 +65,12 @@ exports.createNestedJSON = function (rows, nestingOptions) {
                 else
                     relationKey = levels[i-a].key;
 
-                value = objects[j][relationKey];
-                indexes = getIndex(targetBucket, relationKey, value);
+                value = object[relationKey];
+                indexes = getIndexes(targetBucket, relationKey, value);
                 a++;
             }
 
-            // relation to another object not found
+            // relation to upper object not found - ignore this object
             if(indexes == null || indexes.length == 0)
                 continue;
 
@@ -91,10 +78,39 @@ exports.createNestedJSON = function (rows, nestingOptions) {
                 if(!targetBucket[indexes[z]][relationTable])
                     targetBucket[indexes[z]][relationTable] = new Array();
 
-                targetBucket[indexes[z]][relationTable].push(objects[j]);
+                targetBucket[indexes[z]][relationTable].push(object);
             }
         }
     }
 
+    // at the end, we have all the nested objects in the first bucket
     return buckets[0];
 };
+
+// Returns indexes of an object that has given key and value in objects array
+var getIndexes = function(objects, key, value){
+
+    if (objects == null || key == null || value == null)
+        return null;
+
+    var result = new Array();
+
+    for (var i = 0; i < objects.length; i++) {
+        if(objects[i][key] == value){
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+// Checks if key and value exists in one of the objects in given array
+var isExist = function(array, key, value){
+
+    for (var i = 0; i < array.length; i++) {
+        if(array[i][key] == value)
+            return true;
+    }
+
+    return false;
+}
